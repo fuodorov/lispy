0d3752ebf9f56a7a677b2a8c3d569ca02d4a211c
Slava <slava.fuodorov@yandex.ru>
2025-11-29 07:07:17 +0000

    Small fixes

--- lispy.py
+++ lispy.py
@@ -60,32 +60,6 @@ def repl(prompt='> '):
 def lispstr(exp):
     return '(' + ' '.join(map(lispstr, exp)) + ')' if isinstance(exp, List) else str(exp)
 
-def eval(x, env=global_env):
-    if isinstance(x, Symbol):      # variable reference
-        return env.find(x)[x]
-    elif not isinstance(x, List):  # constant literal
-        return x
-    elif x[0] == 'quote':          # (quote exp)
-        (_, exp) = x
-        return exp
-    elif x[0] == 'if':             # (if test conseq alt)
-        (_, test, conseq, alt) = x
-        exp = (conseq if eval(test, env) else alt)
-        return eval(exp, env)
-    elif x[0] == 'define':         # (define var exp)
-        (_, var, exp) = x
-        env[var] = eval(exp, env)
-    elif x[0] == 'set!':           # (set! var exp)
-        (_, var, exp) = x
-        env.find(var)[var] = eval(exp, env)
-    elif x[0] == 'lambda':         # (lambda (var...) body)
-        (_, parms, body) = x
-        return Procedure(parms, body, env)
-    else:                          # (proc arg...)
-        proc = eval(x[0], env)
-        args = [eval(exp, env) for exp in x[1:]]
-        return proc(*args)
-        
 class Procedure(object):
     def __init__(self, parms, body, env):
         self.parms, self.body, self.env = parms, body, env
@@ -138,6 +112,32 @@ global_env.update({
     'symbol?': lambda x: isinstance(x, Symbol),
     **vars(math), # sin, cos, sqrt, pi, ...
 })
+
+def eval(x, env=global_env):
+    if isinstance(x, Symbol):      # variable reference
+        return env.find(x)[x]
+    elif not isinstance(x, List):  # constant literal
+        return x
+    elif x[0] == 'quote':          # (quote exp)
+        (_, exp) = x
+        return exp
+    elif x[0] == 'if':             # (if test conseq alt)
+        (_, test, conseq, alt) = x
+        exp = (conseq if eval(test, env) else alt)
+        return eval(exp, env)
+    elif x[0] == 'define':         # (define var exp)
+        (_, var, exp) = x
+        env[var] = eval(exp, env)
+    elif x[0] == 'set!':           # (set! var exp)
+        (_, var, exp) = x
+        env.find(var)[var] = eval(exp, env)
+    elif x[0] == 'lambda':         # (lambda (var...) body)
+        (_, parms, body) = x
+        return Procedure(parms, body, env)
+    else:                          # (proc arg...)
+        proc = eval(x[0], env)
+        args = [eval(exp, env) for exp in x[1:]]
+        return proc(*args)  
 
 if __name__ == '__main__':
     repl()
