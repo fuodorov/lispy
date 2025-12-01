07db4c7b9b7e78421c1c71bbdd2d3e23da019531
Slava <slava.fuodorov@yandex.ru>
2025-12-01 02:47:52 +0000

    Add small comments in init module

--- lispy/__init__.py
+++ lispy/__init__.py
@@ -19,8 +19,10 @@ from .primitives import add_globals
 from .repl import load, parse, repl  # noqa: F401
 from .types import EOF_OBJECT, Atom, Exp, Symbol  # noqa: F401
 
+# Initialize the global environment with standard procedures
 add_globals(global_env)
 
+# Define 'and' macro in the global environment
 eval(parse("""(begin
 (define-macro and (lambda args
    (if (null? args) #t
